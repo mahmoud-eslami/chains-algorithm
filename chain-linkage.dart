fec961d690253a8d9bdb7f0b561a9a4b4f394841
mahmoud <eslamimahmoud98@gmail.com>
2022-06-12 18:20:44 +0430

    inital commit

---
+++ chain-linkage.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  // entry point of application
+
+  print("**********************************************");
+  print("**********************************************");
+  print("*****            Chain Linkage           *****");
+  print("**********************************************");
+  print("*****       Author : Mahmoud Eslami      *****");
+  print("**********************************************");
+  print("**********************************************");
+
+  // Reading name of the Geek
+  String? input = stdin.readLineSync();
+
+  print(input);
+}
+
+int chianLinckageMethod(List<int> chains) {
+  return 0;
+}
+
+int getMaxChain(List chains) => 0;
+
+List<int> parseInputToList(String input) => [];
